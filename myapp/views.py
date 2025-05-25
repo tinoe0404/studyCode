e29600f44208e5bdb1ee1dee8c67e64439ff41d1
tinoe0404 <tinoe0404@gmail.com>
2025-05-25 22:20:23 +0200

    feat: add full CRUD functionality for Room model
    - Add RoomForm for create/update
    - Add room_form.html template
    - Add delete confirmation page
    - Update views for create, read, update, delete
    - Modify home.html to list rooms

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 #rooms = [
@@ -16,3 +17,35 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'myapp/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Don't forget to import redirect
+    else:
+        form = RoomForm()
+    context = {'form': form}
+    return render(request, 'myapp/room_form.html', context)
+
+def updateRoom(request, pk):
+  room = Room.objects.get(id=pk)
+  form = RoomForm(instance=room)
+
+  if request.method == 'POST':
+      form = RoomForm(request.POST, instance=room)
+      if form.is_valid():
+          form.save()
+          return redirect('home')
+      
+  context = {'form' : form}
+  return render(request, 'myapp/room_form.html', context) 
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method =='POST':
+        room.delete()
+        return redirect('home')   
+    return render(request, 'myapp/delete.html', {'obj':room})
