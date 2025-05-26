3cb30e9f8853112e02ad945a25fdc5d381cdad80
tinoe0404 <tinoe0404@gmail.com>
2025-05-26 21:28:16 +0200

    Secure room management and fix template bugs
    - Fixed request.user typo in home.html template
    - Implemented proper host verification for room actions
    - Added authentication checks in views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout as auth_logout
@@ -7,7 +9,11 @@ from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
+
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST': 
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -50,8 +56,10 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'myapp/room.html', context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -62,9 +70,14 @@ def createRoom(request):
     context = {'form': form}
     return render(request, 'myapp/room_form.html', context)
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!')
+      
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -77,6 +90,9 @@ def updateRoom(request, pk):
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!')
     if request.method == 'POST':
         room.delete()
         return redirect('home')   
