00b6c6fba7775736d0adfa0bfd55130b285f6ecf
tinoe0404 <tinoe0404@gmail.com>
2025-05-26 20:48:00 +0200

    Fix authentication flow and UI updates
    - Resolve RecursionError in logout view by implementing proper auth_logout
    - Ensure loginPage view returns HttpResponse in all cases
    - Update navbar template with conditional auth links
    - Add success redirects after login/logout actions
    - Improve form validation feedback in login view

--- myapp/views.py
+++ myapp/views.py
@@ -2,20 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout as auth_logout
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
-#rooms = [
-#    {'id':1, 'name':'lets learn python'},
-#    {'id':2, 'name':'Design with  me'},
-#   {'id':3, 'name':'Fronted developers'},
-#]
-
-
 def loginPage(request):
-    if request.method =='POST': 
+    if request.method == 'POST': 
         username = request.POST.get('username')
         password = request.POST.get('password')
 
@@ -24,32 +17,35 @@ def loginPage(request):
         except: 
             messages.error(request, "User does not exist") 
 
-        user = authenticate (request, username=username, password= password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
             messages.error(request, "Username OR password does not exist")
-
-               
-
+    
+    # This needs to be at the same indentation level as the if statement above
     context = {}
-    return render(request, 'myapp/login_register.html', context )
+    return render(request, 'myapp/login_register.html', context)
+
+def user_logout(request):
+    auth_logout(request)
+    return redirect('home')
 
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q') !=None else ''
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-        )
-    topics =Topic.objects.all() 
+    )
+    topics = Topic.objects.all() 
     room_count = rooms.count()
-    context = {'rooms': rooms,'topics': topics, 'room_count': room_count }
-    return render(request, 'myapp/home.html',context)
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'myapp/home.html', context)
 
-def room(request,pk):
+def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'myapp/room.html', context)
@@ -60,28 +56,28 @@ def createRoom(request):
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Don't forget to import redirect
+            return redirect('home')
     else:
         form = RoomForm()
     context = {'form': form}
     return render(request, 'myapp/room_form.html', context)
 
 def updateRoom(request, pk):
-  room = Room.objects.get(id=pk)
-  form = RoomForm(instance=room)
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
 
-  if request.method == 'POST':
-      form = RoomForm(request.POST, instance=room)
-      if form.is_valid():
-          form.save()
-          return redirect('home')
-      
-  context = {'form' : form}
-  return render(request, 'myapp/room_form.html', context) 
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+          
+    context = {'form': form}
+    return render(request, 'myapp/room_form.html', context) 
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    if request.method =='POST':
+    if request.method == 'POST':
         room.delete()
         return redirect('home')   
-    return render(request, 'myapp/delete.html', {'obj':room})
+    return render(request, 'myapp/delete.html', {'obj': room})
