ee64ed52aa421802d16809927fa91cffd80af274
tinoe0404 <tinoe044@gmail.com>
2025-06-02 06:30:24 +0200

    chatroom CRUD functions done

--- myapp/views.py
+++ myapp/views.py
@@ -68,6 +68,8 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -75,9 +77,10 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'myapp/room.html', context)
 
 @login_required(login_url='login')
@@ -122,3 +125,14 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')   
     return render(request, 'myapp/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')   
+    return render(request, 'myapp/delete.html', {'obj': message})
