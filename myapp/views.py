320e48fcb82e8b9967459eea5c80afb7f64dce1f
tinoe0404 <tinoe044@gmail.com>
2025-07-04 14:09:54 +0200

    added activity mobile responsiveness functionality

--- myapp/views.py
+++ myapp/views.py
@@ -198,3 +198,7 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'myapp/topics.html', {'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'myapp/activity.html', {'room_messages': room_messages})
